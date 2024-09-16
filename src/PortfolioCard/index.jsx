067b39f0ff1d404c26405ef8fd6d1c4ce9b7023b
justkuper <kbank1988@gmail.com>
2024-09-16 15:00:18 -0700

    removed hover from portfolio and replaced with ribbons

--- src/PortfolioCard/index.jsx
+++ src/PortfolioCard/index.jsx
@@ -5,7 +5,7 @@ export default function PortfolioCard({data}) {
     return (
         <div className="PortfolioCard">
             <img src = {data.img} alt = {data.title}/>
-            <div className="overlay">
+            <div className="overlay, ribbon">
                 <div className="links">
                 <a href={data.deploy} target="_blank"> <h3>{data.title}</h3></a>
                 <a href={data.link} target="_blank">repo</a>
