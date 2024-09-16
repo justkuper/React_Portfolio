5d1f2c839cb2eca1977d4475935372eae787301c
justkuper <kbank1988@gmail.com>
2024-09-15 20:55:36 -0700

    linkedin and github icons added. fontawesome added to depens in json

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -1,4 +1,6 @@
 import React, { useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import "./style.css";
 export default function Footer() {
     return (
@@ -6,8 +8,18 @@ export default function Footer() {
             <h3>Made with ❤️ and Create React App</h3>
             <h5>&copy; Kuper Bank 2024</h5>
             <ul>
-                <li><a href="https://github.com/justkuper">Github</a></li>
-                <li><a href="https://www.linkedin.com/in/kuperbank/">linkedIn</a></li>
+                <li><a href="https://github.com/justkuper">
+                <FontAwesomeIcon icon={faGithub} flip size="3x" style={{ color: 'green' }} />
+                </a>
+                <br/>
+                <a href="https://github.com/justkuper">
+              <span className="d-inline-block mx-1 name">[ github ]</span>
+            </a>
+            </li>
+                <li><a href="https://www.linkedin.com/in/kuperbank/">
+                <FontAwesomeIcon icon={faLinkedin} flip size="3x" style={{ color: 'green' }} />
+                <br/>
+                [ linkedIn ]</a></li>
             </ul>
         </footer>
     )
