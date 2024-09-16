2488af888ea675f37bfe4b1177ac72492a1c9018
justkuper <kbank1988@gmail.com>
2024-09-15 21:03:26 -0700

    margin and flip added to icons in footer

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
             <h5>&copy; Kuper Bank 2024</h5>
             <ul>
                 <li><a href="https://github.com/justkuper">
-                <FontAwesomeIcon icon={faGithub} flip size="3x" style={{ color: 'green' }} />
+                <FontAwesomeIcon icon={faGithub} flip size="3x" style={{ color: 'green', marginLeft: '15px' }} />
                 </a>
                 <br/>
                 <a href="https://github.com/justkuper">
@@ -17,7 +17,7 @@ export default function Footer() {
             </a>
             </li>
                 <li><a href="https://www.linkedin.com/in/kuperbank/">
-                <FontAwesomeIcon icon={faLinkedin} flip size="3x" style={{ color: 'green' }} />
+                <FontAwesomeIcon icon={faLinkedin} flip size="3x" style={{ color: 'green', marginLeft: '15px' }} />
                 <br/>
                 [ linkedIn ]</a></li>
             </ul>
